6346cf8b6350df43f7e73af54d7fe3ac81ce1872
Shefali Panchal <61265165+shefali310@users.noreply.github.com>
2024-03-01 00:15:21 -0500

    Add hotel completed

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import { error } from "console";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import myHotelRoutes from "./routes/my-hotels";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
@@ -51,3 +52,4 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
