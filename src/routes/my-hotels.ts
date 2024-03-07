83cccaef96f983e80c93eb6c3528423ac710afc5
Shefali Panchal <61265165+shefali310@users.noreply.github.com>
2024-03-07 14:43:38 -0500

    Edit and Delete Api of admin completed.

--- src/routes/my-hotels.ts
+++ src/routes/my-hotels.ts
@@ -16,7 +16,7 @@ const upload = multer({
   },
 });
 
-//api/my-hotels Add hotel api
+// Add hotel api
 router.post(
   "/",
   verifyToken,
@@ -42,14 +42,8 @@ router.post(
       const newHotel: HotelType = req.body;
 
       // 1. upload the image to cloudinary
-      const uploadPromises = imageFiles.map(async (image) => {
-        const b64 = Buffer.from(image.buffer).toString("base64");
-        let dataURI = "data:" + image.mimetype + ";base64," + b64;
-        const res = await cloudinary.v2.uploader.upload(dataURI);
-        return res.url;
-      });
 
-      const imageUrls = await Promise.all(uploadPromises);
+      const imageUrls = await uploadImages(imageFiles);
       // 2. if upload is successful, add the urls to the new hotel
 
       newHotel.imageUrls = imageUrls;
@@ -69,13 +63,96 @@ router.post(
   }
 );
 
-// view hotels api
+// View hotels api
 router.get("/", verifyToken, async (req: Request, res: Response) => {
   try {
     const hotels = await Hotel.find({ userId: req.userId });
     res.json(hotels);
   } catch (error) {
     res.status(500).json({ message: "Error fetching hotels!" });
+  }
+});
+
+// Edit hotel api with also viewing hotel in detail with id.
+
+router.get("/:id", verifyToken, async (req: Request, res: Response) => {
+  const id = req.params.id.toString();
+
+  try {
+    const hotels = await Hotel.findOne({ _id: id, userId: req.userId });
+    res.json(hotels);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching hotels!" });
+  }
+});
+
+// After editing hotel, it will save in the database as well as upload images into cloudinary.
+
+router.put(
+  "/:hotelId",
+  verifyToken,
+  upload.array("imageFiles"),
+  async (req: Request, res: Response) => {
+    try {
+      const updatedHotel: HotelType = req.body;
+      updatedHotel.lastUpdated = new Date();
+
+      const hotel = await Hotel.findOneAndUpdate(
+        {
+          _id: req.params.hotelId,
+          userId: req.userId,
+        },
+        updatedHotel,
+        { new: true }
+      );
+
+      if (!hotel) {
+        return res.status(404).json({ message: "Hotel not found" });
+      }
+
+      const files = req.files as Express.Multer.File[];
+      const updatedImageUrls = await uploadImages(files);
+
+      hotel.imageUrls = [
+        ...updatedImageUrls,
+        ...(updatedHotel.imageUrls || []),
+      ];
+
+      await hotel.save();
+
+      res.status(201).json(hotel);
+    } catch (error) {
+      res.status(500).json({ message: "Something went throw" });
+    }
+  }
+);
+
+// Upload images to cloudinary
+async function uploadImages(imageFiles: Express.Multer.File[]) {
+  const uploadPromises = imageFiles.map(async (image) => {
+    const b64 = Buffer.from(image.buffer).toString("base64");
+    let dataURI = "data:" + image.mimetype + ";base64," + b64;
+    const res = await cloudinary.v2.uploader.upload(dataURI);
+    return res.url;
+  });
+
+  const imageUrls = await Promise.all(uploadPromises);
+  return imageUrls;
+}
+
+// Delete a specific hotel by ID
+router.delete("/:id", verifyToken, async (req, res) => {
+  const id = req.params.id.toString();
+
+  try {
+    const deletedHotel = await Hotel.findOneAndDelete({ _id: id, userId: req.userId });
+    if (!deletedHotel) {
+      return res.status(404).json({ message: "Hotel not found" });
+    }
+    res.json({ message: "Hotel deleted successfully", deletedHotel });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting hotel" });
   }
 });
 
